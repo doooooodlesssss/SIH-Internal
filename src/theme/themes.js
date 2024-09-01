d67e8b4c450c576ae965a2c8e206642282e85cf0
doooooodlesssss <bangera.diya@gmail.com>
2024-09-01 13:36:01 +0530

    ui changes

---
+++ src/theme/themes.js
@@ -0,0 +1,67 @@
+// themes.js
+export const theme = {
+    colors: {
+      primary: '#3880ff',
+      primaryRgb: [56, 128, 255],
+      primaryContrast: '#ffffff',
+      primaryContrastRgb: [255, 255, 255],
+      primaryShade: '#3171e0',
+      primaryTint: '#4c8dff',
+  
+      secondary: '#3dc2ff',
+      secondaryRgb: [61, 194, 255],
+      secondaryContrast: '#ffffff',
+      secondaryContrastRgb: [255, 255, 255],
+      secondaryShade: '#36abe0',
+      secondaryTint: '#50c8ff',
+  
+      tertiary: '#5260ff',
+      tertiaryRgb: [82, 96, 255],
+      tertiaryContrast: '#ffffff',
+      tertiaryContrastRgb: [255, 255, 255],
+      tertiaryShade: '#4854e0',
+      tertiaryTint: '#6370ff',
+  
+      success: '#2dd36f',
+      successRgb: [45, 211, 111],
+      successContrast: '#ffffff',
+      successContrastRgb: [255, 255, 255],
+      successShade: '#28ba62',
+      successTint: '#42d77d',
+  
+      warning: '#ffc409',
+      warningRgb: [255, 196, 9],
+      warningContrast: '#000000',
+      warningContrastRgb: [0, 0, 0],
+      warningShade: '#e0ac08',
+      warningTint: '#ffca22',
+  
+      danger: '#eb445a',
+      dangerRgb: [235, 68, 90],
+      dangerContrast: '#ffffff',
+      dangerContrastRgb: [255, 255, 255],
+      dangerShade: '#cf3c4f',
+      dangerTint: '#ed576b',
+  
+      dark: '#222428',
+      darkRgb: [34, 36, 40],
+      darkContrast: '#ffffff',
+      darkContrastRgb: [255, 255, 255],
+      darkShade: '#1e2023',
+      darkTint: '#383a3e',
+  
+      medium: '#92949c',
+      mediumRgb: [146, 148, 156],
+      mediumContrast: '#ffffff',
+      mediumContrastRgb: [255, 255, 255],
+      mediumShade: '#808289',
+      mediumTint: '#9d9fa6',
+  
+      light: '#f4f5f8',
+      lightRgb: [244, 245, 248],
+      lightContrast: '#000000',
+      lightContrastRgb: [0, 0, 0],
+      lightShade: '#d7d8da',
+      lightTint: '#f5f6f9',
+    },
+  };
